98dcc37fa4c7ca548f7d0704c174614ed71fbb83
Abel D Mathews <70122080+the-hack3rboy@users.noreply.github.com>
2021-06-02 22:16:20 +0530

    Add files via upload

---
+++ Password.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+public class  Password
+{
+   public static void main(String args[])
+     {
+         int length=10;
+         System.out.println("Generating Password using random():");
+         System.out.println("Your new pssword is: " + geek_Password(length));
+     }
+    static char[] geek_Password(int len)
+     {
+         
+         String Capital_letters= "ABCDEFGHIJKLMNOPQRSTUVWXYZ" ;
+         String Small_letters= "abcdefghijklmnopqrstuvwxyz" ;
+         String numbers= "0123456789";
+         String symbols= "!@#$%^&*_-=+?/.<>";
+
+         String values= Capital_letters + Small_letters + numbers + symbols;
+         Random rndm_method = new Random();
+         char[] password = new char[len];
+
+         for (int i=0; i<len; i++)
+              password[i]=values.charAt(rndm_method.nextInt(values.length()));
+         
+         return password;
+     }
+}
